a60510dba9e8aba19cfe0653521de2708b914dfa
Michael Kirk <michael.code@endoftheworl.de>
2021-05-27 13:55:11 -0700

    write point geometries

--- src/rust/src/file_writer.rs
+++ src/rust/src/file_writer.rs
@@ -1,29 +1,32 @@
-use flatbuffers::FlatBufferBuilder;
+use flatbuffers::{FlatBufferBuilder, WIPOffset};
 
 use std::io::Write;
 
-use crate::{FeatureBuilder, GeometryBuilder};
+use crate::{FeatureBuilder, Geometry};
 
 // TODO:
 // -[x] write empty fgb
-// -[ ] write fgb w/ single point
 // -[ ] write un-indexed fgb w/ features
 //   -[ ] columns
 //     -[ ] fixed schema
 //     -[ ] schema-less
 //   -[ ] geometries
-//     -[ ] one geometry type
+//     -[x] one geometry type
 //     -[ ] mixed geometries
 // -[ ] write indexed fgb
 // -[ ] write from FeatureIterator, rather than slice
 // -[ ] write very large fgb
 
 trait FeatureSource {
-    fn build_geometry<'a, 'b>(&self, geometry_builder: &mut GeometryBuilder<'a, 'b>);
+    fn build_geometry<'a>(
+        &'a self,
+        flatbuffer_builder: &mut FlatBufferBuilder<'a>,
+    ) -> WIPOffset<Geometry<'a>>;
 }
 
 #[derive(Debug)]
 struct Writer<'w, W: Write> {
+    include_index: bool,
     inner: &'w mut W,
     bytes_written: usize,
 }
@@ -34,6 +37,7 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 impl<'w, W: Write> Writer<'w, W> {
     pub fn new(writer: &'w mut W) -> Self {
         Self {
+            include_index: false,
             inner: writer,
             bytes_written: 0,
         }
@@ -63,6 +67,11 @@ impl<'w, W: Write> Writer<'w, W> {
 
         let mut header = HeaderBuilder::new(&mut fbb);
         header.add_features_count(features_count as u64);
+        if self.include_index {
+            // TODO
+        } else {
+            header.add_index_node_size(0);
+        }
 
         // TODO: header.add_envelope(&extent);
 
@@ -78,10 +87,7 @@ impl<'w, W: Write> Writer<'w, W> {
     fn write_features(&mut self, features: &[impl FeatureSource]) -> Result<()> {
         for feature_source in features {
             let mut fbb = FlatBufferBuilder::new();
-            let mut geometry_builder = GeometryBuilder::new(&mut fbb);
-            feature_source.build_geometry(&mut geometry_builder);
-            let geometry = geometry_builder.finish();
-
+            let geometry = feature_source.build_geometry(&mut fbb);
             let mut feature_builder = FeatureBuilder::new(&mut fbb);
             feature_builder.add_geometry(geometry);
             let feature = feature_builder.finish();
@@ -95,23 +101,33 @@ impl<'w, W: Write> Writer<'w, W> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{FgbReader, GeometryBuilder, GeometryType};
+    use crate::{FgbReader, Geometry, GeometryBuilder, GeometryType};
+    use flatbuffers::WIPOffset;
 
-    struct MyPoint {
-        xy: [f64; 2],
+    struct MyCoord { x: f64, y: f64 }
+    struct MyPoint(MyCoord);
+    impl MyPoint {
+        fn as_vec(&self) -> Vec<f64> {
+            vec![self.0.x, self.0.y]
+        }
     }
 
     impl FeatureSource for MyPoint {
-        fn build_geometry<'a, 'b>(&self, builder: &mut GeometryBuilder<'a, 'b>) {
-            builder.add_type_(GeometryType::Point);
-            // FIXME: how to add vector? Do I really need to manually coerce the [u8]?
-            // builder.add_xy(flatbuffers::Vector::new(&self.xy, 0));
+        fn build_geometry<'a>(
+            &'a self,
+            flatbuffer_builder: &mut FlatBufferBuilder<'a>,
+        ) -> WIPOffset<Geometry<'a>> {
+            let xy = flatbuffer_builder.create_vector(&self.as_vec());
+            let mut geometry_builder = GeometryBuilder::new(flatbuffer_builder);
+            geometry_builder.add_xy(xy);
+            geometry_builder.add_type_(GeometryType::Point);
+            geometry_builder.finish()
         }
     }
 
     #[test]
     fn test_write_features() {
-        let input: Vec<MyPoint> = vec![MyPoint { xy: [1.0, 2.0] }, MyPoint { xy: [3.0, 4.0] }];
+        let input: Vec<MyPoint> = vec![MyPoint(MyCoord { x: 1.0, y: 2.0}), MyPoint(MyCoord { x: 3.0, y:4.0 })];
 
         let mut output: Vec<u8> = vec![];
         {
@@ -137,7 +153,7 @@ mod tests {
         assert_eq!(false, header.hasTM());
         assert!(header.columns().is_none());
         assert_eq!(2, header.features_count());
-        assert_eq!(16, header.index_node_size());
+        assert_eq!(0, header.index_node_size());
         assert_eq!(None, header.crs());
         assert_eq!(None, header.title());
         assert_eq!(None, header.description());
@@ -147,6 +163,15 @@ mod tests {
         let count = reader.select_all().unwrap();
         assert_eq!(2, count);
         assert_eq!(2, reader.features_count());
+
+        use fallible_streaming_iterator::FallibleStreamingIterator;
+        let mut coords = vec![];
+        while let Some(next) = reader.next().unwrap() {
+            let geometry = next.geometry().unwrap();
+            assert_eq!(GeometryType::Point, geometry.type_());
+            coords.push(geometry.xy().unwrap().safe_slice().to_vec());
+        }
+        assert_eq!(vec![vec![1.0, 2.0], vec![3.0, 4.0]], coords);
     }
 
     #[test]
@@ -178,7 +203,7 @@ mod tests {
         assert_eq!(false, header.hasTM());
         assert!(header.columns().is_none());
         assert_eq!(0, header.features_count());
-        assert_eq!(16, header.index_node_size());
+        assert_eq!(0, header.index_node_size());
         assert_eq!(None, header.crs());
         assert_eq!(None, header.title());
         assert_eq!(None, header.description());

b5ac383001dd7723ccda05ea7b6d3a50cc3f10a3
Michael Kirk <michael.code@endoftheworl.de>
2021-05-31 15:06:34 -0700

    DO NOT MERGE: skip irrelevant builds for now

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -21,50 +21,12 @@ const plugins = [
 ]
 
 export default [{
-  input: './lib/mjs/generic.js',
-  output: {
-    file: 'dist/flatgeobuf.min.js',
-    format: 'umd',
-    name: 'flatgeobuf',
-    sourcemap: true
-  },
-  plugins
-},{
   input: './lib/mjs/geojson.js',
   output: {
     file: 'dist/flatgeobuf-geojson.min.js',
     format: 'umd',
     name: 'flatgeobuf',
     sourcemap: true
-  },
-  plugins
-}, {
-  input: './lib/mjs/ol.js',
-  external: [
-    'ol/Feature',
-    'ol/geom/Point',
-    'ol/geom/MultiPoint',
-    'ol/geom/LineString',
-    'ol/geom/MultiLineString',
-    'ol/geom/Polygon',
-    'ol/geom/MultiPolygon',
-    'ol/geom/GeometryLayout'
-  ],
-  output: {
-    file: 'dist/flatgeobuf-ol.min.js',
-    format: 'umd',
-    name: 'flatgeobuf',
-    sourcemap: true,
-    globals: {
-      'ol/Feature': 'ol.Feature',
-      'ol/geom/Point': 'ol.geom.Point',
-      'ol/geom/MultiPoint': 'ol.geom.MultiPoint',
-      'ol/geom/LineString': 'ol.geom.LineString',
-      'ol/geom/MultiLineString': 'ol.geom.MultiLineString',
-      'ol/geom/Polygon': 'ol.geom.Polygon',
-      'ol/geom/MultiPolygon': 'ol.geom.MultiPolygon',
-      'ol/geom/GeometryLayout': 'ol.geom.GeometryLayout'
-    }
   },
   plugins
 }]
